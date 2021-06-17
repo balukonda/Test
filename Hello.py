c59e1b5792b71c9ce05bf93afdc42dfbe2146442
Konda Balaji <balukonda@yahoo.com>
2021-06-17 17:39:03 +0530

    add python files

---
+++ Hello.py
@@ -0,0 +1,25 @@
+##Test Project##
+
+def fun_print(data):
+    print(data)
+
+
+def fun_sub():
+    data11: int = 13
+    data21: int = 32
+    result: int = data21 - data11
+
+    print(result)
+
+
+def fun_sum():
+    data1 = 2
+    data2 = 3
+
+    print(data1 + data2)
+
+
+if __name__ == '__main__':
+    fun_print('I am Balaji')
+    fun_sum()
+    fun_sub()
